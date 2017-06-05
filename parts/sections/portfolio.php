88aee75afb8bf437caaad4e5c7220247f2c49f9c
Shawn Rieger <riegersn@gmail.com>
2017-06-05 00:58:47 -0400

    move 'collaboration' to out of portfolio and into work section

--- parts/sections/portfolio.php
+++ parts/sections/portfolio.php
@@ -14,25 +14,6 @@
   <div class="header-image"></div>
   <div class="content">
     <h3 class="heading">portfolio</h3>
-    <p>During my work with Boxee and D-Link I've had the privilage of working and colaborating with, building apps and integrating content for some really amazing companies. Just to name a few...</p>
-    <div class="collaborators image-group text-center">
-      <ul class="list-inline">
-        <li><img src="img/work/colaborate_pandora.png" alt="Pandora" /></li>
-        <li><img src="img/work/colaborate_ted.png" alt="TED" height="30px" /></li>
-        <li><img src="img/work/colaborate_mlb.png" alt="MLB.tv" /></li>
-        <li><img src="img/work/colaborate_crackle.png" alt="Crackle" /></li>
-        <li><img src="img/work/colaborate_universal.png" alt="Universal Studios" /></li>
-        <li><img src="img/work/colaborate_nuelion.png" alt="Nuelion" /></li>
-        <li><img src="img/work/colaborate_showtime.png" alt="Showtime" /></li>
-        <li><img src="img/work/colaborate_revision3.png" alt="Revision3" /></li>
-        <li><img src="img/work/colaborate_crunchyroll.png" alt="Crunchyroll" /></li>
-        <li><img src="img/work/colaborate_mubi.png" alt="MUBI" /></li>
-        <li><img src="img/work/colaborate_gbtv.png" alt="GBTV" /></li>
-        <li><img src="img/work/colaborate_bb.png" alt="BoingBoing" /></li>
-        <li><img src="img/work/colaborate_ch.png" alt="CollegeHumor" /></li>
-        <li><img src="img/work/colaborate_howcast.png" alt="Howcast" /></li>
-      </ul>
-    </div>
     <div class="text-center">
       <h3><small>Project list comming soon...</small></h3>
     </div>
--- parts/sections/work.php
+++ parts/sections/work.php
@@ -26,5 +26,24 @@
         <li><img src="img/work/employer_commercebank.png" alt="Commerce Bank" /></li>
       </ul>
     </div>
+    <p>During my work with Boxee and D-Link I've had the privilage of working and colaborating with, building apps and integrating content for some really amazing companies. Just to name a few...</p>
+    <div class="collaborators image-group text-center">
+      <ul class="list-inline">
+        <li><img src="img/work/colaborate_pandora.png" alt="Pandora" /></li>
+        <li><img src="img/work/colaborate_ted.png" alt="TED" height="30px" /></li>
+        <li><img src="img/work/colaborate_mlb.png" alt="MLB.tv" /></li>
+        <li><img src="img/work/colaborate_crackle.png" alt="Crackle" /></li>
+        <li><img src="img/work/colaborate_universal.png" alt="Universal Studios" /></li>
+        <li><img src="img/work/colaborate_nuelion.png" alt="Nuelion" /></li>
+        <li><img src="img/work/colaborate_showtime.png" alt="Showtime" /></li>
+        <li><img src="img/work/colaborate_revision3.png" alt="Revision3" /></li>
+        <li><img src="img/work/colaborate_crunchyroll.png" alt="Crunchyroll" /></li>
+        <li><img src="img/work/colaborate_mubi.png" alt="MUBI" /></li>
+        <li><img src="img/work/colaborate_gbtv.png" alt="GBTV" /></li>
+        <li><img src="img/work/colaborate_bb.png" alt="BoingBoing" /></li>
+        <li><img src="img/work/colaborate_ch.png" alt="CollegeHumor" /></li>
+        <li><img src="img/work/colaborate_howcast.png" alt="Howcast" /></li>
+      </ul>
+    </div>
   </div>
 </section>
