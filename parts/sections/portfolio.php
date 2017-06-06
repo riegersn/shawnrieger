98bb53783847a2e4f8fa70e9ce444fd6b02cc727
Shawn <riegersn@gmail.com>
2017-06-05 23:47:14 -0400

    Adding portfolio items

--- parts/sections/portfolio.php
+++ parts/sections/portfolio.php
@@ -14,8 +14,47 @@
   <div class="header-image"></div>
   <div class="content">
     <h3 class="heading">portfolio</h3>
-    <div class="text-center">
-      <h3><small>Project list comming soon...</small></h3>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project hm-zoom" data-name="pandora">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/pandora/pandora_thumb.png" alt="Pandora Radio">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="revision3">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/revision3/revision3_thumb.png" alt="Revision3">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="mlb">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/mlb/mlb_thumb.png" alt="MLB">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="nhl">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/nhl/nhl_thumb.png" alt="NHL">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="ted">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/ted/ted_thumb.png" alt="TED">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="suicidegirls">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/suicidegirls/suicidegirls_thumb.png" alt="SuicideGirls">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="accuweather">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/accuweather/accuweather_thumb.png" alt="Accuweather">
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4">
+        <div class="project" data-name="gbtv">
+            <img class="img-rounded thumbnail" src="/img/portfolio/apps/gbtv/gbtv_thumb.png" alt="GBTV">
+        </div>
+    </div>
     </div>
   </div>
 </section>
