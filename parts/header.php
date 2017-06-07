0ed36276195421efe0b120b52bac012ff5e11f9a
Shawn <riegersn@gmail.com>
2017-06-07 19:19:09 -0400

    Set page title

--- parts/header.php
+++ parts/header.php
@@ -7,10 +7,18 @@
    * Default page header
    */
 
+   $defaults = array(
+     'title' => 'Shawn Rieger | Application/Front-End Developer | Welcome'
+   );
+
+   if (isset($_SESSION)) {
+     $defaults = array_merge($defaults, $_SESSION);
+   }
+
 ?>
 
 <head>
- <title>ShawnRieger.com: Welcome</title>
+ <title><?php echo $defaults['title']; ?></title>
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Rubik:500|Merriweather:300" rel="stylesheet">
