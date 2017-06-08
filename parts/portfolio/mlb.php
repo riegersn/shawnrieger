5b0b6b4f1419da7d8fbcb032cb1cf97db5dc8f8a
Shawn Rieger <riegersn@gmail.com>
2017-06-08 11:55:13 -0400

    mlb portfolio text / copy from pandora

---
+++ parts/portfolio/mlb.php
@@ -0,0 +1,44 @@
+<?php
+
+  /**
+   * parts/portfolio/pandora.php
+   * by riegersn
+   *
+   * Portfolio Section / Pandora
+   */
+
+?>
+
+<!-- about the company/service -->
+<p>MLB.tv is an American subscription based audio and video service which through two different service tiers allows users to hear and watch HD quality Major League Baseball games live via a high-speed Internet connection. Watch every out-of-market regular season game live or on demand on your favorite supported devices. Subscribers receive In-Game highlights and stats, Live game DVR control, Full Game Archives, Audio overlay (Away radio audio overlay is available to MLB.tv Premium subscribers only), Pitch by Pitch widget, Clickable linescores and Fantasy player tracker.</p>
+
+<!-- divider -->
+<hr>
+
+<!-- about the project -->
+<p>The Story</p>
+<p>All in all the Pandora application supported the following features and requirements...</p>
+
+<!-- list of features, important info -->
+<ul>
+  <li>Basics. Full play, pause, skip support</li>
+  <li>Thumbs up/down support &amp; tracking</li>
+  <li>Station creation from currently playing artist, track, song, and search.</li>
+  <li>Station deletion.</li>
+  <li>Full search. Artist, album, song.</li>
+  <li>Skip tracking. Implementing Pandora's skipping rules.</li>
+  <li>Pandora ad support</li>
+  <li>Device linking between the Boxee TV device and the Pandora service</li>
+  <li>Pandora user account authentication</li>
+</ul>
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<ul class="list-inline">
+  <li><a href="https://github.com/riegersn/mlb-bbx" target="_blank"><img class="git-icon" src="/img/general/github.png" alt="">View on Github</a></li>
+</ul>
+
+<!-- divider -->
+<hr>
