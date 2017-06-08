46211137bcaae2456f881e9a43ace9b9b02d15ac
Shawn <riegersn@gmail.com>
2017-06-08 16:48:27 -0400

    Revision3 portfolio text

---
+++ parts/portfolio/revision3.php
@@ -0,0 +1,32 @@
+<?php
+
+  /**
+   * parts/portfolio/revision3.php
+   * by riegersn
+   *
+   * Portfolio Section / Revision3
+   */
+
+?>
+
+<!-- about the company/service -->
+<p>Revision3 was a San Francisco-based multi-channel Internet television network that creates, produces and distributes Web television shows on niche topics. Founded in 2005, it has been operating as a subsidiary of Discovery Digital Networks since 2012. The network has primarily technology-based shows hosted and produced by Patrick Norton, and gaming shows hosted and produced by Adam Sessler. The network also creates and hosts comedic, political, DIY, and movie-related content. The name refers to the revisioning of video programming, according to founders Jay Adelson, Kevin Rose and David Prager. </p>
+
+<!-- divider -->
+<hr>
+
+<!-- about the project -->
+<p>The Revision3 application was a straight forward, one 'page' application designed myself from the ground up utilizing <code>Photoshop</code>, <code>JavaScript</code>, and <code>QML</code>. It featured 2 rows with a hidden 3<sup>rd</sup> that was only visible when selecting a series title.</p> The top row was a 'jumbotron' or 'showcase' list that contained featured episodes and videos. The video thumbnails would slide from side to side depending on which direction you'd press the remote, the focus would always remain centered.</p>
+<p>The 2<sup>nd</sup> row contained all available Revision3 series. The entire list would shift from right to left while focus remaied centered similar to the featured shows. When you clicked the select button on the remote, the entire screen would slide up to reveal that series episodes.</p>
+<p>I've always been a fan and big supporter of Revision3 and all of the <a href="https://en.wikipedia.org/wiki/TechTV" target="_blank">TechTV</a> guys, way back to the days of <a href="https://en.wikipedia.org/wiki/The_Screen_Savers" target="_blank">The Screen Savers</a> TV show. So I always made sure their content had a home at Boxee.</p>
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<ul class="list-inline">
+  <li><a href="https://github.com/riegersn/revision3-btv" target="_blank"><img class="git-icon" src="/img/general/github.png" alt="">View on Github</a></li>
+</ul>
+
+<!-- divider -->
+<hr>
