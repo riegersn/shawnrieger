e5322efe50aafc31e9e32c7909737614768c0ce3
Shawn <riegersn@gmail.com>
2017-06-10 15:53:44 -0400

    Coming soon

---
+++ parts/portfolio/gbtv.php
@@ -0,0 +1,54 @@
+<?php
+
+  /**
+   * parts/portfolio/gbtv.php
+   * by riegersn
+   *
+   * Portfolio Section / GBTV
+   */
+
+?>
+
+<!-- about the company/service -->
+<p>Details coming soon...</p>
+
+<!-- divider -->
+<!-- <hr> -->
+
+<!-- about the project -->
+<!-- <p>When I was tasked with building a new MLB.TV application, I was estatic. I love the sport and was thrilled to updated my older, and outdated application I previously built for the original Boxee Box. I was also able to work with the same team from MLB which was great.</p>
+<p>MLB.TV utilized HLS (HTTP Live Streaming). developed by Apple in 2009. This gave them the ability to steam live video in stunning hd quality, far exceeding any technology that has come before it. Similar to my <a href="/portfolio/pandora">Pandora</a> application, I utilized <code>JavaScript</code> &amp; <code>QML</code> to acheave a beautiful final result. Unfortunately due to time restrictions and client limitations some more advaced features of MLB.TV such as multi-game view where not possible.</p> -->
+
+<!-- list of features, important info -->
+<!-- <h3><small>MLB.TV Application Features</small></h3>
+<ul>
+  <li>Daily schedules with live scores.</li>
+  <li>Playback of both home &amp; away video streams.</li>
+  <li>Audio selection, when available, for each stream. (radio, ballpark only...).</li>
+  <li>Caleder view showing past &amp; future games</li>
+  <li>Sandings view of current leauge stadings for all teams.</li>
+  <li>Ability to select your favorite team(s).</li>
+  <li>Set score visibility on, off, or only off for your favorite teams' scores.</li>
+  <li>Video quality could be set to HD, SD or Adaptive based on your connection.</li>
+</ul> -->
+
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<ul class="list-inline">
+  <li><code>XUL</code></li>
+  <li><code>Python</code></li>
+</ul>
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<!-- <ul class="list-inline">
+  <li><a href="https://github.com/riegersn/mlb-btv" target="_blank"><img class="git-icon" src="/img/general/github.png" alt="">View on Github</a></li>
+</ul> -->
+
+<!-- divider -->
+<!-- <hr> -->
