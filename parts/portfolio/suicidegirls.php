3c4bbf764c10947e4b0cbb7d0a3dc74ed98ea2a3
Shawn <riegersn@gmail.com>
2017-06-10 10:21:48 -0400

    suicidegirls

---
+++ parts/portfolio/suicidegirls.php
@@ -0,0 +1,42 @@
+<?php
+
+  /**
+   * parts/portfolio/suicidegirls.php
+   * by riegersn
+   *
+   * Portfolio Section / SuicideGirls
+   */
+
+?>
+
+<!-- about the company/service -->
+<p>SuicideGirls is an online community based website that revolves around pin-up photography sets of models known as the Suicide Girls. The website was founded in 2001 by Selena Mooney ("Missy Suicide") and Sean Suhl ("Spooky"). Most of the site is only accessible to paying members. It offers members access to images provided by models and photographers worldwide, as well as personal profiles, blogging platforms and the option to join numerous groups based upon different interests. There is also an online merchandise store offering a range of clothing, books and DVDs.</p>
+
+<!-- divider -->
+<hr>
+
+<!-- about the project -->
+<p>The SuicideGirls application was fun to build as the UI was designed to match the look &amp; feel of the their brand (at that time). The app featured full SuicideGirls goodness with all the videos, models and photo sets your heart desired. Starting off with a free set of pics and videos, users could log in with their accounts directly inside the application and get access to the entire sites media content on a big screen TV. Browse through all the models and their photos, sort and search, see the newest content and more. </p>
+<p>The app was officially demoed and launched at the Boxee Meetup NYC event at Webster Hall in Brooklyn NY to great applause, although some of the SuicideGirls where there on stage stealing the spotlight!</p>
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<ul class="list-inline">
+  <li><code>XUL</code></li>
+  <li><code>Python</code></li>
+  <li><code>PHP</code></li>
+  <li><code>XML</code></li>
+</ul>
+
+<!-- divider -->
+<hr>
+
+<!-- list of links -->
+<ul class="list-inline">
+  <li><a href="https://github.com/riegersn/suicidegirls-bbx" target="_blank"><img class="git-icon" src="/img/general/github.png" alt="">View on Github</a></li>
+</ul>
+
+<!-- divider -->
+<hr>
--- parts/sections/work.php
+++ parts/sections/work.php
@@ -15,15 +15,14 @@
   </div>
   <div class="content">
     <h3 class="heading">work</h3>
-    <p>I started my career in the tech field working for <a href="#">Commerce Bank</a> and <a href="#">TD Bank NA</a> as an IT Help Desk agent. I got my foot in the door but it wasn't my passion. I continued coding in VB6 on the side for fun until early 2009, I discovered the small startup <a href="#">Boxee</a>.</p>
-    <p>Boxee was a desktop "media-center" software turned set-top device based off <a href="#">XBMC</a>, currating video content from around the web. Their software supported building applications using XUL and Python. I quickly built a PBS and GameTrailers app. Threw up <a href="#">BoxeeHQ.com</a> to host my applications in a repository.</p>
+    <p>I started my career in the tech field working as an IT Help Desk agent for <a href="https://www.tdbank.com/" target="_blank">TD Bank NA</a>, working my way up to a Level 2 tech after my first year. I got my foot in the door but it wasn't my passion. I continued coding in VB6 on the side for fun until early 2009, I discovered the small startup <a href="http://www.boxee.tv/" target="_blank">Boxee</a>.</p>
+    <p>Boxee was a desktop "media-center" software turned set-top device based off <a href="https://en.wikipedia.org/wiki/XBMC" target="_blank">XBMC</a>, currating video content from around the web. Their software supported building applications using XUL and Python. I quickly built a PBS and GameTrailers app. Threw up BoxeeHQ.com (transfered to a fellow Boxee enthusiast when I was hired) to host my applications in a repository.</p>
     <p>Come mid-June of 09, my apps where gaining traction with the community, when I received a email from the Boxee guys about my work. I joined the Boxee team and continued to write apps for the platform over the next 4 years.</p>
-    <div class="employers image-group text-center">
+    <div class="employers image-group-large text-center">
       <ul class="list-inline">
-        <li><img src="img/work/employer_boxee.png" alt="Boxee Inc." /></li>
         <li><img src="img/work/employer_dlink.png" alt="D-Link Systems Inc." /></li>
+        <li><img src="img/work/employer_boxee.png" alt="Boxee Inc." /></li>
         <li><img src="img/work/employer_tdbankna.png" alt="TD Bank NA" /></li>
-        <li><img src="img/work/employer_commercebank.png" alt="Commerce Bank" /></li>
       </ul>
     </div>
     <h3><small>colaborations</small></h3>
