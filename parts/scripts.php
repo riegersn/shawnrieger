1a71e39c55fd71ce162010fac4d6ce433f58fec6
Shawn <riegersn@gmail.com>
2017-06-10 15:54:27 -0400

    Absolute path

--- parts/scripts.php
+++ parts/scripts.php
@@ -15,4 +15,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/4.1.2/imagesloaded.min.js"
   integrity="sha256-VC4gpGBDKiyXJhHygvqwm6pHHF11cmXTXal2qHqTMmA=" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js/main.min.js"></script>
+<script type="text/javascript" src="/js/main.min.js"></script>
