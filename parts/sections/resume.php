bb0c12c9cc6b8ab44601b57a9f06a5b3187bf571
Shawn <riegersn@gmail.com>
2017-06-13 16:40:25 -0400

    Updated resume

--- parts/sections/resume.php
+++ parts/sections/resume.php
@@ -11,17 +11,17 @@
 ?>
 
 <section id="resume">
-  <div class="content resume">
+  <div class="content">
     <a href="/downloads/shawn-rieger-2017.pdf" download><button type="button" class="btn btn-primary pull-right resume-download-btn" data-name="resume-download">Download PDF</button></a>
     <h3 class="heading">Résumé</h3>
-      <h4 class="resume-header">Shawn N. Rieger</h4>
-      <h4 class="resume-header text-center"><strong>Application Developer | Software Project Management</strong></h4>
-      <p>Innovative and diversified Software Application Developer, with a solid history of design and development expertise. Talent for identifying, isolating and resolving coding issues and analyzing end-user needs to align capabilities with overall objectives. Regarded as an innovator, developing software solutions for high-growth organizations like Sony, Pandora, and MLB, and creating a stronger posture within competitive markets.</p>
+      <h4>Shawn N. Rieger</h4>
+      <h4 class="text-center"><strong>Application Developer | Software Project Management</strong></h4>
+      <p>Innovative and diversified Software Application Developer, with a solid history of design and development expertise. Talent for identifying, isolating and resolving coding issues and analyzing end-user needs to align capabilities with overall objectives. Accomplished developer, building software solutions for high-growth organizations like Sony, Pandora, and MLB, and creating a stronger posture within competitive markets.</p>
       <div class="row">
         <div class="col-sm-6 col-md-4">
           <ul>
             <li>Software Development</li>
-            <li>JavaScript, jQuery, HTML, CSS, XML</li>
+            <li>JavaScript, jQuery, XUL, HTML, CSS</li>
             <li>Quality Assurance &amp; Testing</li>
           </ul>
         </div>
@@ -41,29 +41,35 @@
         </div>
       </div>
       <hr />
-      <h4 class="resume-header">Career Experience</h4>
-        <p><strong>D-LINK SYSTEMS, INC.</strong>, Fountain Valley, CA<br><em>Demonstrated exceptional finesse working autonomously, driving production of robust software solutions, managing the technical-end of multiple set-top boxes and developed and maintained code utilizing JavaScript, Qt, Python and XML.</em></p>
-        <h5 class="resume-job-title">SENIOR APPLICATION DEVELOPER - TELECOMMUTE (2013 –2017)</h5>
+      <h4>Career Experience</h4>
+        <h5>D-LINK SYSTEMS, INC. <small>Fountain Valley, CA</small></h5>
+        <p><em>Demonstrated exceptional finesse working autonomously, driving production of robust software solutions, managing the technical-end of multiple set-top boxes and developed and maintained code utilizing JavaScript, Qt, Python and XUL.</em></p>
+        <h6>SENIOR APPLICATION DEVELOPER - TELECOMMUTE (2013 –2017)</h6>
         <ul>
           <li>Led technical support operations, designed application UI mockups and assets, utilizing Photoshop. Modified code, resolving errors and to control potentially negative impacts to the production lifecycle.</li>
           <li>Nurtured partnerships with 3rd party developers to facilitate the development and launch of set-top applications.</li>
         </ul>
-        <p><strong>BOXEE INC.,</strong> New York, NY<br><em>Content manager &amp; lead application developer of native and intuitive set-top applications. Developed PHP web crawlers to aggregate movie and TV data, enabling the design of a more intuitive set-top box.</em></p>
-        <h5 class="resume-job-title">APPLICATION DEVELOPER - TELECOMMUTE (2013 –2017)</h5>
+        <h5>BOXEE INC. <small>New York, NY</small></h5>
+        <p><em>Content manager &amp; lead application developer of native and intuitive set-top applications. Developed PHP web crawlers to aggregate movie and TV data, enabling the design of a more intuitive set-top box.</em></p>
+        <h6>APPLICATION DEVELOPER - TELECOMMUTE (2013 –2017)</h6>
         <ul>
           <li>Worked collaboratively with industry moguls like Sony, MLB, NHL, TED, and Pandora, to pioneer proprietary software solutions that launched them into leading roles within a highly competitive market.</li>
           <li>Developed application API’s, maintaining the API documentation, and created internal web application to validate content XML documents, from partners, integrating video, etc., into the platform.</li>
           <li>Developed and maintained playback control scripts integrating set top box remote to video playback in internal browser utilizing JavaScript.</li>
         </ul>
-        <p><strong>TD BANK NA,</strong> Mount Laurel, NJ<br><em>Worked both in a team setting &amp; autonomously, as the on-call agent, providing multi-tiered troubleshooting. Effectively enhanced resolution times, and managed queries at various threat levels.</em></p>
-        <h5 class="resume-job-title">IT SERVICE DESK | LEVEL 2 REPRESENTATIVE (2007 - 2009)</h5>
+        <h5>TD BANK NA <small>Mount Laurel, NJ</small></h5>
+        <p><em>Worked both in a team setting &amp; autonomously, as the on-call agent, providing multi-tiered troubleshooting. Effectively enhanced resolution times, and managed queries at various threat levels.</em></p>
+        <h6>IT SERVICE DESK | LEVEL 2 REPRESENTATIVE (2007 - 2009)</h6>
         <ul>
           <li>Created a comprehensive knowledge-base, ensuring high levels of proficiency. Consistently updated information and delivered employee training. Redesigned help desk logo using Inkscape vector software.</li>
         </ul>
       <hr />
-      <h4 class="resume-header">Education &amp; Credentials</h4>
-      <p><em><strong>Associate of Applied Science in Computer Information Systems</strong></em>, Camden County College, Blackwood, NJ</p>
-      <p><em><strong>Professional Development Training</strong></em>: Oracle PL/SQL &amp; Oracle DBA Certificate, DBA University Online</p>
-      <p><em><strong>Technical Knowledge</strong></em>: JavaScript, jQuery, HTML, HTML5, CSS, CSS3, Sass, Bootstrap, React, Qt QML, Python, PHP, Bash, SQL, PL/SQL, RESTful API, SOAP, HTTP Live Streaming, OOP, RDBMS, Ajax, JSON, RSS/ATOM, GIT, Node.js, NPM, Gulp, Bower, Photoshop, Illustrator, Inkscape, Pixelmator, GIMP, OSX, Linux, Windows</p>
+      <h4>Education &amp; Credentials</h4>
+      <strong><em>Associate of Applied Science in Computer Information Systems</em></strong><br>
+      <p>Camden County College, Blackwood, NJ</p>
+      <strong><em>Professional Development Training</em></strong><br>
+      <p>Oracle PL/SQL &amp; Oracle DBA Certificate, DBA University Online</p>
+      <strong><em>Technical Knowledge</em></strong><br>
+      <p>JavaScript, jQuery, HTML, HTML5, CSS, CSS3, Sass, Bootstrap, Qt QML, Python, XUL, PHP, Bash, SQL, PL/SQL, RESTful API, SOAP, HTTP Live Streaming, OOP, RDBMS, Ajax, JSON, RSS/ATOM, GIT, Node.js, NPM, Gulp, Bower, Photoshop, Illustrator, Inkscape, Pixelmator, GIMP, OSX, Linux, Windows</p>
   </div>
 </section>
