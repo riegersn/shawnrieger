20bd3c7ef9dbfd521a3e039c1577fc2df4e4c150
Shawn <riegersn@gmail.com>
2017-06-12 21:18:18 -0400

    removed extra closing div

--- privacy-policy.php
+++ privacy-policy.php
@@ -22,86 +22,85 @@
   <?php include 'parts/nav.php' ?>
 
 <div id="main">
+  <section>
+    <div class="content">
+      <h3>Privacy Policy</h3>
+      <p>This Privacy Policy governs the manner in which ShawnRieger.com collects, uses, maintains and discloses information collected from users (each, a "User") of the http://www.shawnrieger.com website ("Site").</p>
 
-      <section>
-        <div class="content">
-          <h3>Privacy Policy</h3>
-          <p>This Privacy Policy governs the manner in which ShawnRieger.com collects, uses, maintains and discloses information collected from users (each, a "User") of the http://www.shawnrieger.com website ("Site").</p>
+      <h5>Personal identification information</h5>
+      <p>We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
 
-          <h5>Personal identification information</h5>
-          <p>We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+      <h5>Non-personal identification information</h5>
+      <p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
 
-          <h5>Non-personal identification information</h5>
-          <p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+      <h5>Web browser cookies</h5>
+      <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
 
-          <h5>Web browser cookies</h5>
-          <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+      <h5>How we use collected information</h5>
+      <p>ShawnRieger.com may collect and use Users personal information for the following purposes:</p>
+      <ul>
+        <li>
+          <i>To run and operate our Site</i><br/>
+          We may need your information display content on the Site correctly.
+        </li>
+      </ul>
 
-          <h5>How we use collected information</h5>
-          <p>ShawnRieger.com may collect and use Users personal information for the following purposes:</p>
-          <ul>
-            <li>
-              <i>To run and operate our Site</i><br/>
-              We may need your information display content on the Site correctly.
-            </li>
-          </ul>
+      <h5>How we protect your information</h5>
+      <p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
 
-          <h5>How we protect your information</h5>
-          <p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
+      <h5>Sharing your personal information</h5>
+      <p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
 
-          <h5>Sharing your personal information</h5>
-          <p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
+      <p>We may share or sell information with third parties for marketing or other purposes.</p>
 
-          <p>We may share or sell information with third parties for marketing or other purposes.</p>
+      <h5>Sharing your personal information</h5>
+      <p>This Privacy Policy governs the manner in which ShawnRieger.com collects, uses, maintains and discloses information collected from users (each, a "User") of the http://www.shawnrieger.com website ("Site").</p>
 
-          <h5>Sharing your personal information</h5>
-          <p>This Privacy Policy governs the manner in which ShawnRieger.com collects, uses, maintains and discloses information collected from users (each, a "User") of the http://www.shawnrieger.com website ("Site").</p>
+      <h5>Personal identification information</h5>
+      <p>We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
 
-          <h5>Personal identification information</h5>
-          <p>We may collect personal identification information from Users in a variety of ways in connection with activities, services, features or resources we make available on our Site. Users may visit our Site anonymously. We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+      <h5>Non-personal identification information</h5>
+      <p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
 
-          <h5>Non-personal identification information</h5>
-          <p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+      <h5>Web browser cookies</h5>
+      <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
 
-          <h5>Web browser cookies</h5>
-          <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+      <h5>How we use collected information</h5>
+      <p>ShawnRieger.com may collect and use Users personal information for the following purposes:</p>
+      <ul>
+        <li>
+          <i>To run and operate our Site</i><br/>
+          We may need your information display content on the Site correctly.
+        </li>
+      </ul>
 
-          <h5>How we use collected information</h5>
-          <p>ShawnRieger.com may collect and use Users personal information for the following purposes:</p>
-          <ul>
-            <li>
-              <i>To run and operate our Site</i><br/>
-              We may need your information display content on the Site correctly.
-            </li>
-          </ul>
+      <h5>How we protect your information</h5>
+      <p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
 
-          <h5>How we protect your information</h5>
-          <p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
+      <h5>Sharing your personal information</h5>
+      <p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
 
-          <h5>Sharing your personal information</h5>
-          <p>We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. </p>
+      <p>We may share or sell information with third parties for marketing or other purposes.</p>
 
-          <p>We may share or sell information with third parties for marketing or other purposes.</p>
+      <h5>Third party websites</h5>
+      <p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.</p>
 
-          <h5>Third party websites</h5>
-          <p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website's own terms and policies.</p>
+      <h5>Changes to this privacy policy</h5>
+      <p>ShawnRieger.com has the discretion to update this privacy policy at any time. When we do, we will post a notification on the main page of our Site. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
 
-          <h5>Changes to this privacy policy</h5>
-          <p>ShawnRieger.com has the discretion to update this privacy policy at any time. When we do, we will post a notification on the main page of our Site. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+      <h5>Your acceptance of these terms</h5>
+      <p>By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
 
-          <h5>Your acceptance of these terms</h5>
-          <p>By using this Site, you signify your acceptance of this policy. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
+      <h5>Contacting us</h5>
+      <p>If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us.</p>
 
-          <h5>Contacting us</h5>
-          <p>If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us.</p>
-
-          <p>This document was last updated on June 02, 2017</p>
-        </div>
-      </div>
-    </section>
+      <p>This document was last updated on June 02, 2017</p>
+    </div>
+  </section>
 </div>
-    <!-- Include the footer -->
-    <?php include 'parts/footer.php'; ?>
+
+<!-- Include the footer -->
+<?php include 'parts/footer.php'; ?>
 
 </body>
 </html>
